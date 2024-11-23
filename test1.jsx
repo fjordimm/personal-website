072ec7a245fc22341a4afcd01b59d7efb4a2e69a
fjordimm <fjordimm@gmail.com>
2024-11-22 17:53:18 -0800

    testing jsx

---
+++ test1.jsx
@@ -0,0 +1,13 @@
+// const mainSection = document.getElementById("the-main-section");
+// const bruhContent = document.createTextNode("howdy ho");
+// const bruh = document.createElement("h1");
+// bruh.appendChild(bruhContent);
+// mainSection.appendChild(bruh);
+
+const mainSection = document.getElementById("the-main-section");
+const root = ReactDOM.createRoot(mainSection);
+root.render(<p>howdy there me amigo</p>);
+
+// const app = document.getElementById('app');
+// const root = ReactDOM.createRoot(app);
+// root.render(<h1>Develop. Preview. Ship.</h1>);
